a2a62da98136b0e588000f33da35d7e55b5c51eb
fabiiann1 <fabianmarinn1@gmail.com>
2025-02-27 15:56:43 -0500

    cambio_prueba

--- gestion_tareas/gestion_tareas/settings.py
+++ gestion_tareas/gestion_tareas/settings.py
@@ -71,9 +71,7 @@ WSGI_APPLICATION = 'gestion_tareas.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-
+# https://docs.djangoproject.com/en/5.1/ref/settings/#datab
 
 DATABASES = {
     'default': {
