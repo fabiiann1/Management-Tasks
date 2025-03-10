4b93d07f8a5f1407fc13139fb846389694d5a8e4
fabiiann1 <fabianmarinn1@gmail.com>
2025-03-10 15:47:31 -0500

    Urls Tokens
    Incluí las rutas de registro y login

--- gestion_tareas/app_tareas/urls.py
+++ gestion_tareas/app_tareas/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
+from .views import RegisterView, CustomAuthToken
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/login/', CustomAuthToken.as_view(), name='register'),
+
 ]
