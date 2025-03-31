556f4183c691f874e9f4b0e7d9821f03afe162cd
ivan alejandro cosme <147445770+IVAN-LAMUTE@users.noreply.github.com>
2025-03-31 11:53:06 -0500

    modelos modificados

--- gestion_tareas/app_tareas/models.py
+++ gestion_tareas/app_tareas/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from django.db.models import TextChoices
 
 # Create your models here.
 class Task(models.Model):
-    STATE_CHOICES = (
-        ('BACKLOG', 'Backlog'),
-        ('TO DO', 'To Do'),
-        ('DOING', 'Doing'),
-        ('TEST', 'Test'),
-        ('DONE', 'Done'),
-    )
-    
-    PRIORITY_CHOICES = (
-        ('ALTA', 'Alta'),
-        ('MEDIA', 'Media'),
-        ('BAJA', 'Baja'),
-    )
+    class StateChoices(TextChoices):
+        BACKLOG = 'BACKLOG', 'Backlog'
+        TO_DO = 'TO DO', 'To Do'
+        DOING = 'DOING', 'Doing'
+        TEST = 'TEST', 'Test'
+        DONE = 'DONE', 'Done'
+
+    class PriorityChoices(TextChoices):
+        HIGH = 'ALTA', 'Alta'
+        MEDIUM = 'MEDIA', 'Media'
+        LOW = 'BAJA', 'Baja'
     
     name = models.CharField(max_length=200, verbose_name="Nombre de la tarea")
     description = models.TextField(verbose_name="Descripción de la tarea")
-    state = models.CharField(max_length=10,choices=STATE_CHOICES,default='BACKLOG',verbose_name="Estado")
-    priority = models.CharField(max_length=5,choices=PRIORITY_CHOICES,default='MEDIA',verbose_name="Prioridad")
+    state = models.CharField(max_length=10,choices=StateChoices.choices,
+    default=StateChoices.BACKLOG,verbose_name="Estado")
+    priority = models.CharField(max_length=5,choices=PriorityChoices.choices, default=PriorityChoices.MEDIUM,verbose_name="Prioridad")
     due_date = models.DateField(verbose_name="Fecha de entrega")
     comment = models.TextField(blank=True, null=True, verbose_name="Comentario")
     assigned_user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='assigned_tasks',verbose_name="Usuario asignado")
@@ -38,4 +37,3 @@ class Task(models.Model):
     
     def __str__(self):
         return self.name
-
